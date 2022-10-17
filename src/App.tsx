f5908fa809dac0505ca6b6248fb90b913deeeebc
Lucas Gimenes <lucas_sin5@hotmail.com>
2022-10-16 21:24:50 -0300

    after labs 1/2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import { useState } from "react";
+import { SignIn } from "./pages/SignIn";
 import "./styles/global.css";
 
 export function App() {
-  return (
-    <div>
-      <h3 className="text-2xl font-bold text-violet-500">Hello World</h3>
-
-      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300">
-        Enviar
-      </button>
-    </div>
-  );
+  return <SignIn />;
 }
